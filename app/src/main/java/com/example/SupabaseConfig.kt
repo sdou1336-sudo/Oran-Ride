d65a9845c9a2614abcd4d0fa51bf5db1d7126c8d
YourName <your@email.com>
2026-07-20 11:29:54 +0100

    Setup Supabase client

--- app/src/main/java/com/example/SupabaseConfig.kt
+++ app/src/main/java/com/example/SupabaseConfig.kt
@@ -1,6 +1,6 @@
 package com.example
 
 object SupabaseConfig {
-    const val URL = "https://omebkgnkhlyoapctfvgy.supabase.co"
-    const val KEY = "sb_publishable_RWXSF1e-9VwQscoWroJqeg_s0AkA2sm"
+    const val URL="https://omebkgnkhlyoapctfvgy.supabase.co"
+    const val KEY="ضع_المفتاح_هنا"
 }
