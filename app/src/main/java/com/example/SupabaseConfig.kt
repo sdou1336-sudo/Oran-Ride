320362be0722fd0c154f8085dba2f11519e9d74e
YourName <your@email.com>
2026-07-20 11:25:30 +0100

    Add Supabase configuration

---
+++ app/src/main/java/com/example/SupabaseConfig.kt
@@ -0,0 +1,6 @@
+package com.example
+
+object SupabaseConfig {
+    const val URL = "https://omebkgnkhlyoapctfvgy.supabase.co"
+    const val KEY = "sb_publishable_RWXSF1e-9VwQscoWroJqeg_s0AkA2sm"
+}
