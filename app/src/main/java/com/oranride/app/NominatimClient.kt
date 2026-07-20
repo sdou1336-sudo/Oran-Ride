8975ba92932d528b39b81c52d4734c33ef49389f
YourName <your@email.com>
2026-07-20 22:12:30 +0100

    Fix Moshi converter for Nominatim

--- app/src/main/java/com/oranride/app/NominatimClient.kt
+++ app/src/main/java/com/oranride/app/NominatimClient.kt
@@ -2,13 +2,15 @@ package com.oranride.app
 
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 
 object NominatimClient {
 
     val api: NominatimApi by lazy {
         Retrofit.Builder()
             .baseUrl("https://nominatim.openstreetmap.org/")
-            .addConverterFactory(MoshiConverterFactory.create())
+            .addConverterFactory(MoshiConverterFactory.create(Moshi.Builder().add(KotlinJsonAdapterFactory()).build()))
             .build()
             .create(NominatimApi::class.java)
     }
